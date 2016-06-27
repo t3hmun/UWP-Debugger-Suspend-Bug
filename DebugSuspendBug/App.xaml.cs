53f8fe33147d8f8a7c3ae50ea1423119d9bd81c4
t3hmun <t3hmun_at_gmail>
2016-06-27 16:42:14 +0100

    Added a DispatcherTimer, which does sucessfully suspend. Also comments.

--- DebugSuspendBug/App.xaml.cs
+++ DebugSuspendBug/App.xaml.cs
@@ -1,78 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
+﻿// Debugger Suspend button fail test code.
+// This app demonstrates the debuggers failure to suspend a System.Threading.Timer when the App-Lifecycle Suspend button is pressed.
+// The non-indented comments show where the changes from the default blank app are.
+// All changes are in this file.
 namespace DebugSuspendBug
 {
+    using System;
     using System.Diagnostics;
     using System.Threading;
+    using Windows.ApplicationModel;
+    using Windows.ApplicationModel.Activation;
     using Windows.UI.Notifications;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Navigation;
+    using Microsoft.ApplicationInsights;
 
     /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
+    ///     Provides application-specific behavior to supplement the default Application class.
     /// </summary>
     sealed partial class App : Application
     {
 //Timer object.
-        System.Threading.Timer _timer;
+        private Timer _systemThreadingTimer;
+        private DispatcherTimer _dispatcherTimer;
 
         /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
+        ///     Initializes the singleton application object.  This is the first line of authored code
+        ///     executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
         public App()
         {
-            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
-                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
-                Microsoft.ApplicationInsights.WindowsCollectors.Session);
-            this.InitializeComponent();
-            this.Suspending += OnSuspending;
+            WindowsAppInitializer.InitializeAsync(
+                WindowsCollectors.Metadata |
+                WindowsCollectors.Session);
+            InitializeComponent();
+            Suspending += OnSuspending;
         }
 
 // A simple toast method.
         private void Toast(string text)
-        { 
+        {
             var toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText04);
             toastXml.GetElementsByTagName("text")[0].AppendChild(toastXml.CreateTextNode(text));
             ToastNotificationManager.CreateToastNotifier().Show(new ToastNotification(toastXml));
         }
 
         /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
+        ///     Invoked when the application is launched normally by the end user.  Other entry points
+        ///     will be used such as when the application is launched to open a specific file.
         /// </summary>
         /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
 //Timer that runs when the app is suspended by the debugger button but not when suspended when run from the start menu.
-            _timer = new Timer(x =>
+            _systemThreadingTimer = new Timer(x =>
             {
-                var s = DateTime.Now.Second.ToString();
+                var s = $"ThreadTimer: {DateTime.Now.Second.ToString()}";
                 Toast(s);
-                Debug.WriteLine($"Timer: {s}");
-            }, null,0, 2000);
+                Debug.WriteLine(s);
+            }, null, 0, 2000);
+//A DispatcherTimer that does the same thing. This will suspend properly when the debugger suspend button is pressed.
+            _dispatcherTimer = new DispatcherTimer() { Interval = TimeSpan.FromSeconds(2) };
+            _dispatcherTimer.Tick += ((x,y) =>
+            {
+                var s = $"DispatcherTimer: {DateTime.Now.Second.ToString()}";
+                Toast(s);
+                Debug.WriteLine(s);
+            });
+            _dispatcherTimer.Start();
 
-#region unmodified-code
+            #region unmodified-code
+
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 this.DebugSettings.EnableFrameRateCounter = true;
             }
 #endif
-            Frame rootFrame = Window.Current.Content as Frame;
+            var rootFrame = Window.Current.Content as Frame;
 
             // Do not repeat app initialization when the Window already has content,
             // just ensure that the window is active
@@ -104,23 +109,24 @@ namespace DebugSuspendBug
                 // Ensure the current window is active
                 Window.Current.Activate();
             }
-#endregion
+
+            #endregion
         }
 
         /// <summary>
-        /// Invoked when Navigation to a certain page fails
+        ///     Invoked when Navigation to a certain page fails
         /// </summary>
         /// <param name="sender">The Frame which failed navigation</param>
         /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        private void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
         }
 
         /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
+        ///     Invoked when application execution is being suspended.  Application state is saved
+        ///     without knowing whether the application will be terminated or resumed with the contents
+        ///     of memory still intact.
         /// </summary>
         /// <param name="sender">The source of the suspend request.</param>
         /// <param name="e">Details about the suspend request.</param>
